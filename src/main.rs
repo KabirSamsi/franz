40a78df433e392fd33f54d157c6ab756d46bb811
Kabir Samsi <samsikabir@gmail.com>
2024-08-06 02:06:54 -0400

    So I can parse and compile notes all the way now. Yet somehow I still can't parse a dot.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,91 @@ mod error;
 mod parse;
 // mod preprocess;
 mod songs;
+use crate::ast::NoteComp;
 
 macro_rules! notes {
     ($name:ident, $speed:expr) => {
         let _ =
             &codegen::compile_seq(stringify!($name), songs::$name(), $speed);
     };
+}
+
+fn try_to_build_a_song() {
+    let husn = NoteComp::Phrase(vec![
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, qt.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, qt.)"),
+        parse_assistant!(NoteParser, "(d4, qt.)"),
+        parse_assistant!(NoteParser, "(c4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(a4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(e4, qt.)"),
+        parse_assistant!(NoteParser, "(g4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(g4, et.)"),
+        parse_assistant!(NoteParser, "(a4, hf.)"),
+        parse_assistant!(NoteParser, "(g4, qt)"),
+        parse_assistant!(NoteParser, "(a4, et.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(e4, et.)"),
+        parse_assistant!(NoteParser, "(d4, qt.)"),
+        parse_assistant!(NoteParser, "(d4, hf.)"),
+        parse_assistant!(NoteParser, "(d4, hf.)"),
+        parse_assistant!(NoteParser, "(d4, hf.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, qt.)"),
+        parse_assistant!(NoteParser, "(d4, et.)"),
+        parse_assistant!(NoteParser, "(f4, qt.)"),
+        parse_assistant!(NoteParser, "(d4, hf.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(a4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(e4, qt.)"),
+        parse_assistant!(NoteParser, "(g4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, et.)"),
+        parse_assistant!(NoteParser, "(g4, qt.)"),
+        parse_assistant!(NoteParser, "(g4, et.)"),
+        parse_assistant!(NoteParser, "(b4_flt, hf.)"),
+        parse_assistant!(NoteParser, "(a4, qt)"),
+        parse_assistant!(NoteParser, "(g4, et.)"),
+        parse_assistant!(NoteParser, "(a4, et.)"),
+        parse_assistant!(NoteParser, "(g4, et.)"),
+        parse_assistant!(NoteParser, "(f4, qt.)"),
+        parse_assistant!(NoteParser, "(f4, hf.)"),
+        parse_assistant!(NoteParser, "(f4, hf.)"),
+        parse_assistant!(NoteParser, "(f4, hf.)"),
+    ]);
+
+    let _ = codegen::compile_seq("husn", husn, 0.25);
 }
 
 fn main() {
@@ -27,8 +106,9 @@ fn main() {
     parse_assistant!(ParamParser, "meter = 4/4;");
     parse_assistant!(ParamParser, "tempo = Andante;");
 
+    try_to_build_a_song();
     notes![innocent, 0.25];
     notes![anthem, 0.3];
     notes![apprasionata, 0.15];
-    notes![darthvader, 0.3];
+    notes![imperial_march, 0.3];
 }
